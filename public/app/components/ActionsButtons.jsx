aab6fefa4d98504c0dd45a8ad76f55b9c86d4d04
Davide Bontempelli <davide.bonte@gmail.com>
2016-06-30 12:32:40 +0200

    add vagrant run backend

--- public/app/components/ActionsButtons.jsx
+++ public/app/components/ActionsButtons.jsx
@@ -21,7 +21,7 @@ export default class ActionsButtons extends React.Component {
         let deleteButton = <DeleteButton key={100} data={this.props.data} getData={this.props.getData}/>;
         let editButton = <Button key={200} bsSize={actionButtonSize} onClick={this.edit}><Glyphicon glyph="wrench"/></Button>;
         let duplicateButton = <DuplicateButton key={300} data={this.props.data} open={this.props.open}/>
-        let runButton = <RunButton key={400} data={this.props.data}><Glyphicon glyph="send"/></RunButton>;
+        let runButton = <RunButton key={400} data={this.props.data} getData={this.props.getData}><Glyphicon glyph="send"/></RunButton>;
 
         switch (status) {
             case 'saved':
@@ -40,10 +40,10 @@ export default class ActionsButtons extends React.Component {
                 break;
             case 'builded':
                 //builded=|delete
-                if(this.props.data.RepoTags){
-                  if (this.props.data.RepoTags.length === 1) {
-                    buttons.push(deleteButton);
-                  }
+                if (this.props.data.RepoTags) {
+                    if (this.props.data.RepoTags.length === 1) {
+                        buttons.push(deleteButton);
+                    }
 
                 }
                 break;
@@ -140,14 +140,24 @@ class RunButton extends React.Component {
     startInstance = () => {
         this.setState({showModal: true});
         console.log(this.props.data.uid);
-        xhr({
-            json: {
+        let json = {};
+        if (this.props.data.type === 'docker') {
+            json = {
                 name: this.state.name,
                 hostIp: this.state.hostIP,
                 hostPort: this.state.hostPort,
                 containerPort: this.state.containerPort,
                 protocol: this.state.protocol
-            },
+            }
+        } else if (this.props.data.type === 'vagrant') {
+          console.log(this.props.data);
+            json = {
+                name: this.state.name,
+                imageName: this.props.data.name
+            }
+        }
+        xhr({
+            json,
             method: 'post',
             uri: baseUrl + '/v1/instances/new' + '?uid=' + this.props.data.uid
 
@@ -166,39 +176,38 @@ class RunButton extends React.Component {
 
     render() {
 
-        return ( < Button key = {
-            400
-        }
-        bsStyle = "success" bsSize = {
-            actionButtonSize
-        } > <Glyphicon glyph="send" onClick={this.open}/> < Modal show = {
-            this.state.showModal
-        }
-        onHide = {
-            this.close
-        } > <Modal.Header closeButton>
-            <Modal.Title>Modal heading</Modal.Title>
-        </Modal.Header> < Modal.Body > <Form inline>
-            <ControlLabel>Container name</ControlLabel>
-            {' '}
-            <FormControl type="text" placeholder="name" size="8" onChange={this.handleFieldChange.bind(this, 'name')}/></Form> < Form inline > <ControlLabel>PortBindings:</ControlLabel>
-            {' '} < FormControl type = "text" placeholder = "Host IP Address" size = "8" onChange = {
-            this.handleFieldChange.bind(this, 'hostIP')
-        } />< FormControl type = "text" placeholder = "Host Port" size = "8" onChange = {
-            this.handleFieldChange.bind(this, 'hostPort')
-        } />< FormControl type = "text" placeholder = "Container Port" size = "8" onChange = {
-            this.handleFieldChange.bind(this, 'containerPort')
-        } /> <FormControl componentClass="select" onChange={this.handleFieldChange.bind(this, 'protocol')}>
-            <option value="">both</option>
-            <option value="tcp">tcp</option>
-            <option value="udp">udp</option>
-        </FormControl> < /Form>
+        let networkForm = undefined;
 
+        if (this.props.data.type === 'docker') {
+            networkForm = <Form inline > <ControlLabel>PortBindings:</ControlLabel>
+            {
+                ' '
+            } < FormControl type = "text" placeholder = "Host IP Address" size = "8" onChange = {
+                this.handleFieldChange.bind(this, 'hostIP')
+            } />< FormControl type = "text" placeholder = "Host Port" size = "8" onChange = {
+                this.handleFieldChange.bind(this, 'hostPort')
+            } />< FormControl type = "text" placeholder = "Container Port" size = "8" onChange = {
+                this.handleFieldChange.bind(this, 'containerPort')
+            } /> <FormControl componentClass="select" onChange={this.handleFieldChange.bind(this, 'protocol')}>
+                <option value="">both</option>
+                <option value="tcp">tcp</option>
+                <option value="udp">udp</option>
+            </FormControl> < /Form>
+      }
 
-
-                            </Modal.Body > <Modal.Footer>
-            <Button onClick={this.startInstance}>Start</Button>
-            <Button onClick={this.close}>Close</Button>
-        </Modal.Footer> < /Modal> < /Button >)
+        return ( < Button bsStyle = "success" bsSize = {actionButtonSize} > <Glyphicon glyph="send" onClick={this.open}/ > < Modal show = {
+                this.state.showModal
+            }
+            onHide = {
+                this.close
+            } > <Modal.Header closeButton>
+                <Modal.Title>Modal heading</Modal.Title>
+            </Modal.Header> < Modal.Body > <Form inline>
+                <ControlLabel>Container name</ControlLabel>
+                {' '}
+                <FormControl type="text" placeholder="name" size="8" onChange={this.handleFieldChange.bind(this, 'name')}/></Form>
+            {                networkForm
+            } < /Modal.Body > <Modal.Footer>
+            <Button onClick={this.startInstance}>Start</Button > <Button onClick={this.close}>Close</Button> < /Modal.Footer> < /Modal > < /Button >)
     }
 }
